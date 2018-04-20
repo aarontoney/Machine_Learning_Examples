5850ef731a3514d63fcaba5e5ab8408f3c31108c
Aaron Toney <joeboy@hhhh.org>
2018-04-19 23:47:21 -0700

    creating simple tensor flow examples - one for math graph and one for text

--- tensorflow/hello_tensorflow/hello_tensorflow.py
+++
@@ -1,8 +0,0 @@
-#-------------------------------------------------------------------------------
-# Example from: https://www.tensorflow.org/install/install_mac
-#-------------------------------------------------------------------------------
-# Python
-import tensorflow as tf
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
-print(sess.run(hello))
